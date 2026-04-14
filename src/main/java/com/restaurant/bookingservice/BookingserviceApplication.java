635c80d54f3fb24d356e5afdcd4b3361df3dad3d
Sasha <alexanderbondarev388@mail.ru>
2026-04-14 10:41:12 +0300

    app

--- src/main/java/com/restaurant/bookingservice/BookingserviceApplication.java
+++ src/main/java/com/restaurant/bookingservice/BookingserviceApplication.java
@@ -3,7 +3,6 @@ package com.restaurant.bookingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableAsync
@@ -11,13 +10,5 @@ public class BookingserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookingserviceApplication.class, args);
     }
-    @GetMapping("/")
-    public String home() {
-        return "Restaurant Booking Service is running!";
-    }
 
-    @GetMapping("/health")
-    public String health() {
-        return "OK";
-    }
 }
